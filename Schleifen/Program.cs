121da5fd8207ed56ed0e33bb19c6f0161c12729a
Andrich83 <144699047+Andrich83@users.noreply.github.com>
2023-09-13 09:53:49 +0200

    Init

---
+++ Schleifen/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Schleifen-Demo");
+int iterCount = 10;
+
+while (iterCount > 0)
+{
+  Console.WriteLine("WHILE");
+  iterCount = iterCount - 1;
+  //iterCount--;
+  // --iterCount; !! ACHTUNG: Nicht das gleiche! Mit vorsicht genießen.
+}
+
+iterCount = 10;
+do
+{
+  Console.WriteLine("DO-WHILE");
+  iterCount--;
+} while (iterCount > 0);
+
+
+Console.WriteLine("FOR-LOOP");
+for (int i = 0; i < 10; i++)
+{
+  Console.WriteLine("FOR");
+}
+
---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Willkommen beim Taschenrechner 3000");
+bool checkParse1 = false;
+bool checkParse2 = false;
+int num1 = 0;
+int num2 = 0;
+while (!checkParse1 || !checkParse2)
+{
+  Console.WriteLine("Bitte gebe Zahl 1 an:");
+  string input1 = Console.ReadLine();
+
+  Console.WriteLine("Bitte gebe Zahl 2 an:");
+  string input2 = Console.ReadLine();
+
+
+  // Konvertierung von Text zu Zahlen
+  checkParse1 = int.TryParse(input1, out num1);
+  checkParse2 = int.TryParse(input2, out num2);
+}
+var result = num1 + num2;
+Console.WriteLine($"{num1} + {num2} = {result}");
+
+
+// Entweder (Var), Oder (Datentyp eg. string, int, float, decimal,...)
+//string test1 = "Buxtehude";
+//int test2 = 69;
+//var test3 = "Buxtehude";
+//test3 = 69;
+//var int x = 420; // "Var" ist nicht kombinierbar mit Datentypenangaben
